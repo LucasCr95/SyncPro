588754136f21b0f76c51ed15d8bc4800be52c2fc
LucasCruz <lucas.crr95@gmail.com>
2023-08-04 01:54:10 -0300

    230804

--- syncpro/src/components/pages/Project.js
+++ syncpro/src/components/pages/Project.js
@@ -8,13 +8,15 @@ import styles from './Project.module.css'
 export default function Project() {
    const { id } = useParams()
    const [ project, setProject ] = useState([])
+   const [ showProjectForm, setShowProjectForm ] = useState(false)
+   const [ showServiceForm, setShowServiceForm ] = useState(false)
 
    useEffect(() => {
       setTimeout(() => {
          fetch(`http://localhost:5000/projects/${ id }`, {
-         method: 'GET',
-         headers: {
-            'Content-Type' : 'application/json'
+            method: 'GET',
+            headers: {
+               'Content-Type' : 'application/json'
             }
          })
          .then(resp => resp.json())
@@ -25,19 +27,54 @@ export default function Project() {
       }, 2000)
    }, [ id ])
 
+   function toggleProjectForm() {
+      setShowProjectForm(!showProjectForm)
+   }
+
+   function toggleServiceForm() {
+      setShowServiceForm(!showServiceForm)
+   }
+
    return (
       <>
          {project.name ? (
             <div className={ styles.project_container }>
-               <div className={ styles.title_container }>
-                  <h1>{ project.name }</h1>
-                  <p><span>Orçamento</span>: { project.budget }</p>
-                  <p><span></span> {project.category.name }</p>
+               <div className={ styles.info_container }>
+                  <div className={ styles.details_container }>
+                     <h1>{ project.name }</h1>
+                     <p><span>Orçamento</span>: { project.budget }</p>
+                     <p><span>Categoria</span>: { project.category.name }</p>
+                     <p><span>Total utilizado</span>: { project.budget }</p>
+                  </div>
+                  <div className={ styles.form_container }>
+                     <button onClick={ toggleProjectForm } className={ styles.btn }>
+                        {!showProjectForm ? 'Editar' : 'Fechar'}
+                     </button>
+                     {showProjectForm && (
+                        <div className={ styles.form_edit }>
+                           <p>Formulario</p>
+                        </div>
+                     )}
+                  </div>
                </div>
-               <div className={ styles.content_container }>
-                  <div className={ styles.btn_container }>
-                     <button>Editar</button>
-                     <button>Adicionar Serviços</button>
+               <div className={ styles.services_container }>
+                  <div className={ styles.details_services }>
+                     <h2>Serviços</h2>
+                     <p>Serviços</p>
+                     <p>Serviços</p>
+                     <p>Serviços</p>
+                     <p>Serviços</p>
+                     <p>Serviços</p>
+                  </div>
+                  <div className={ styles.form_services}>
+                     <button onClick={ toggleServiceForm } className={ styles.btn }>
+                        {!showServiceForm ? 'Adicionar' : 'Fechar'}
+                     </button>
+                     {showServiceForm && (
+                        <div className={ styles.service_edit }>
+                           Formulario de Serviços
+                        </div>
+                     )}
                   </div>
                </div>
             </div>

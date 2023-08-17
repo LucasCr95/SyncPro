847e470f0be012430178804422473ec22f48705d
LucasCruz <lucas.crr95@gmail.com>
2023-08-17 01:57:32 -0300

    230817
    finaliza sessão contact. finaliza projeto

--- syncpro/src/components/pages/Contact.js
+++ syncpro/src/components/pages/Contact.js
@@ -5,28 +5,30 @@ import styles from './Contact.module.css'
 export default function Contact() {
     return (
       <div className={ styles.contact_container }>
-         <div>
+         <div className={ styles.title_container }>
             <h1>Fale conosco</h1>
          </div>
-         <div className={ styles.info_contact_container}>
+         <div className={ styles.info_container}>
             <address>
                <h4>Contatos</h4>
-                  <div>
-                     <a href='mailto:contact@syncpro.com'>
-                        <BsEnvelope />
-                        <p>contact@syncpro.com</p>
-                     </a>
-                  </div>
-                  <div>
+               <ul className={ styles.address_list }>
+                  <li className={ styles.address_item }>
+                     <BsEnvelope />
+                     <p>
+                        <a href='mailto:contact@syncpro.com'>
+                           contact@syncpro.com
+                        </a>
+                     </p>
+                  </li>
+                  <li className={ styles.address_item }>
                      <BsPhone />
                      <p>+55 11 97070-7070</p>
-                  </div>
-               <h5>Endereço</h5>
-               <div>
-                  <BsMap />
-                  <p>Alameda Santos, 1468 - Andar 13</p>
-                  <p>08814-125</p>
-               </div>
+                  </li>
+                  <li className={ styles.address_item }>
+                     <BsMap />
+                     <p>Alameda Santos, 1468 - Andar 13, 08814-125</p>
+                  </li>
+               </ul>
             </address>
          </div>
       </div>    
